1fb2e0da76de32bc0101f29b4e8cd4adad5b774f
hypermania <hypermania@uchicago.edu>
2018-01-20 05:11:03 -0600

    all btags

--- process.py
+++ process.py
@@ -1,13 +1,23 @@
-from io import FileIO
 import json
 import re
 
-_generated_btags = open('./data/generated_btags.txt', 'r')
-generated_btags = _generated_btags.read().split('\n')
-while generated_btags[-1] == '':
-    generated_btags = generated_btags[:-1]
-generated_btags = list(set(generated_btags))
+def load_btag_file(filename):
+    _generated_btags = open(filename, 'r')
+    generated_btags = _generated_btags.read().split('\n')
+    while generated_btags[-1] == '':
+        generated_btags = generated_btags[:-1]
+    return generated_btags
 
-save_file = open('./data/generated_btags_processed.txt', 'w')
-for btag in generated_btags:
-    save_file.write("{}\n".format(btag))
+    
+_wordlist = load_btag_file('./data/wordlist_generated_btags.txt')
+_generator = load_btag_file('./data/generated_btags_reduced.txt')
+_first_name = load_btag_file('./data/first_name_generated_btags.txt')
+_scraped = load_btag_file('./data/btags.txt')
+
+all_btags = _wordlist + _generator + _first_name + _scraped
+all_btags = sorted(list(set(all_btags)))
+
+btags_output = open('./data/btags_sum.txt', 'a')
+for btag in all_btags:
+    btags_output.write("{}\n".format(btag))
+btags_output.close()
